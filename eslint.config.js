a18f8bb56e3a263b99877b2d60a1322799480ed6
student-wroclaw <student-wroclaw@EDU-WSB.local>
2025-06-23 15:59:51 +0200

    Dodaj podstawową konfigurację ESLint w eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+export default [
+
+  {
+
+    files: ["**/*.js"],
+
+    languageOptions: {
+
+      ecmaVersion: 2021,
+
+      sourceType: "module",
+
+    },
+
+    rules: {
+
+      semi: ["error", "always"],
+
+      "no-unused-vars": "warn",
+
+      "no-console": "off"
+
+    },
+
+  },
+
+];
