c1932df12b83d543bfd5297cf09d2782ee7d1d85
student-wroclaw <student-wroclaw@EDU-WSB.local>
2025-06-23 16:01:57 +0200

    Zmień konfigurację ESLint na CommonJS w eslint.config.cjs

---
+++ eslint.config.cjs
@@ -0,0 +1,27 @@
+module.exports = [
+
+  {
+
+    files: ["**/*.js"],
+
+    languageOptions: {
+
+      ecmaVersion: 2021,
+
+      sourceType: "module",
+
+    },
+
+    rules: {
+
+      semi: ["error", "always"],
+
+      "no-unused-vars": "warn",
+
+      "no-console": "off"
+
+    },
+
+  },
+
+];
